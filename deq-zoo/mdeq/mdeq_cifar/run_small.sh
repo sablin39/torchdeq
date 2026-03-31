d49b202daa7512c105d3641d9778f3c0ec6ff15c
sablin39 <1020030829@qq.com>
2026-03-31 21:35:56 +0800

    Drop deq-zoo due to incompatibility with current impl

--- deq-zoo/mdeq/mdeq_cifar/run_small.sh
+++
@@ -1,9 +0,0 @@
-python main.py                  \
-    --cfg ./configs/small.yaml  \
-    --norm_type weight_norm     \
-    --f_solver broyden          \
-    --f_max_iter 22             \
-    --grad 5                    \
-    --tau 0.5                   \
-    --exp_id small              \
-    ${@:1}
